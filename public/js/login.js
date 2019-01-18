63abe912781637776cef57f4c9645c68b17687ae
danny <danny.eeraerts@proximus.be>
2019-01-18 10:18:46 +0100

    change code

--- public/js/login.js
+++ public/js/login.js
@@ -18,19 +18,58 @@ function getFormData(form) {
   return formData;
 }
 
+function handleLoginRequest(event) {
+  var request = event.target;
+
+  if (request.readyState === 4) {
+    console.log(request.status);
+
+    if (request.status >= 200 && request.status < 300) {
+      console.log('succes');
+    } else {
+      alert('How seg das nie just!');
+    }
+  }
+}
+
+function handleRegisterRequest(event) {
+  var request = event.target;
+
+  if (request.readyState === 4) {
+    console.log(request.status);
+
+    if (request.status >= 200 && request.status < 300) {
+      console.log('succes');
+    } else {
+      alert('How seg das nie just!');
+    }
+  }
+}
+
 function login(event) {
   event.preventDefault();
   var formData = getFormData(loginForm);
   var request = new XMLHttpRequest();
-  request.addEventListener('readystatechange', function (event) {
-    console.log(event);
-  });
+  request.addEventListener('readystatechange', handleLoginRequest);
   request.open('POST', 'http://connect4.pienter.space/api/auth/login');
   request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify(formData));
+}
+
+function register(event) {
+  event.preventDefault();
+  var formData = getFormData(registerForm);
+  var request = new XMLHttpRequest();
+  request.addEventListener('readystatechange', handleRegisterRequest);
+  request.open('POST', 'http://connect4.pienter.space/api/auth/register'); // welke methode en welke url naar server
+
+  request.setRequestHeader('Content-Type', 'application/json'); // data is in jason form hoe processen
+
   request.send(JSON.stringify(formData));
 }
 
 goToRegister.addEventListener('click', switchForm);
 goToLogin.addEventListener('click', switchForm);
 loginForm.addEventListener('submit', login);
+registerForm.addEventListener('submit', register);
 //# sourceMappingURL=login.js.map
--- src/js/login.js
+++ src/js/login.js
@@ -8,25 +8,76 @@ function switchForm(event) {
   loginForm.classList.toggle('hide');
   registerForm.classList.toggle('hide');
 }
+
+function hideLoginAlert(){
+  loginAlert.classList.add('hide');
+}
+
+function howloginAlert(content)
+
 function getFormData(form) {
   const inputFields = form.querySelectorAll('input');
   const formData = {};
-  inputFields.forEach(function (inputField) {
+  inputFields.forEach(function(inputField) {
     formData[inputField.name] = inputField.value;
   });
   return formData;
 }
-function login(event){
+function handleLoginRequest(event) {
+  const request = event.target;
+  if (request.readyState === 4) {
+    const response = JSON.parse(request.responseText);
+  }
+    console.log(request.status);
+    if (request.status >= 200 && request.status < 300) {
+
+      console.log('succes');
+    } else if (request.status)===401) {
+       showloginalert.text
+
+
+  }
+}
+
+function handleRegisterRequest(event) {
+  const request = event.target;
+  if (request.readyState === 4) {
+    console.log(request.status);
+    if (request.status >= 200 && request.status < 300) {
+      console.log('succes');
+    } else {
+      alert('How seg das nie just!');
+    }
+  }
+}
+
+
+function login(event) {
   event.preventDefault();
   const formData = getFormData(loginForm);
   const request = new XMLHttpRequest();
-  request.addEventListener('readystatechange', function (event) {
-    console.log(event);
-  });
+  request.addEventListener('readystatechange', handleLoginRequest);
   request.open('POST', 'http://connect4.pienter.space/api/auth/login');
   request.setRequestHeader('Content-Type', 'application/json');
   request.send(JSON.stringify(formData));
 }
+
+function register(event) {
+  event.preventDefault();
+  const formData = getFormData(registerForm);
+  const request = new XMLHttpRequest();
+  request.addEventListener('readystatechange', handleRegisterRequest) ;
+  request.open('POST', 'http://connect4.pienter.space/api/auth/register'); // welke methode en welke url naar server
+  request.setRequestHeader('Content-Type', 'application/json'); // data is in jason form hoe processen
+  request.send(JSON.stringify(formData));
+}
+
+
 goToRegister.addEventListener('click', switchForm);
 goToLogin.addEventListener('click', switchForm);
 loginForm.addEventListener('submit', login);
+registerForm.addEventListener('submit', register);
+loginForm.addEventListener('input', function(event){
+  console.log('input');
+  loginAlert.classList.add('hide');
+}
